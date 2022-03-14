c004b519e74d67a8a4f76a6aaa6750c7ebad3e75
Esra Sertsu <Esra.Sertsu@logo.com.tr>
2022-03-14 08:57:17 +0300

    package js fix 2

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'nprogress/nprogress.css';
-import dateFnsLocalizer from 'react-widgets-date-fns';
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import { tr } from "date-fns/locale";
-import parse from "date-fns/parse";
 
-const locales = {
-  "tr": tr
-};
 
-dateFnsLocalizer({
-  format,
-  parse,
-  getDay,
-  locales
-});
+
 
 
 ReactDOM.render(
